0fd36a53f67f677545a6f2913d67970f17e672eb
Giwby Albatross <giwby.albatross@gmail.com>
2025-02-19 10:47:12 +1100

    Update package-release.sh
    Explain purpose of package-release.sh and fix pycache and ._* file cleanup.

--- package-release.sh
+++ package-release.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/sh
 
+# all this does is put the src and assets into a tar file that can be taken anywhere and unpakced then run
+
 tarfilelocation="releases/release-$1-$2_BETA.tar.gz"
 
 echo "Removing unnessesary files (pycache, ._* files etc.)"
 
-#mkdir ._* /tmp/unnessesary
-#mv  src/__pycache__ /tmp/unnessesary
+mkdir /tmp/unnessesary
+mv */._* src/__pycache__ /tmp/unnessesary
 
 echo "Putting this release's files into a tar file located at $tarfilelocation"
 
