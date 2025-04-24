53053339078e34276c13c489bc6f6b063ef1429a
Giwby Albatross <giwby.albatross@gmail.com>
2025-04-25 09:23:41 +1000

    Update `VERSION` number and add more comments to `src/settings.py`
    Also move definition of `scr_center`

--- src/settings.py
+++ src/settings.py
@@ -1,12 +1,11 @@
 HARDNESS = 1 # medium by default
-TITLE = "AAMI crunching"
-scr_w = 1024
-scr_h = 768
-scr_center = (scr_w // 2, scr_h // 2)
-FPS = 60
+TITLE = "AAMI crunching" # window title
+scr_w = 1024 # # window
+scr_h = 768 #  # size
+FPS = 60 # target FPS
 GFX_MODE = 4 # determines how graphics-intensive this should be
 
-VERSION = 'v1.2.0-ALPHA'
+VERSION = 'v1.2.1-ALPHA'
 
 SHOW_FPS = True
 DEBUG = True  # should be False for release versions
@@ -23,5 +22,7 @@ RENDER_DEBUG_WINDOW = True
 DEBUG = DEBUG and __debug__ # debug calls don't happen in optimised mode
 VERY_VERBOSE = VERY_VERBOSE and DEBUG # can't be very verbose in not debug mode
 RENDER_DEBUG_WINDOW = RENDER_DEBUG_WINDOW and DEBUG # only render debug window in debug mode
-SHOW_FPS = SHOW_FPS and __debug__
+SHOW_FPS = SHOW_FPS and __debug__ # don't how FPS counter in optimised mode
 tinafey_likelihood = 1024 // HARDNESS # specify likelihood that tina will spawn
+GFX_MODE = GFX_MODE if __debug__ else max(GFX_MODE, 3) # dont push graphics too high in optimised mode
+scr_center = (scr_w // 2, scr_h // 2)
