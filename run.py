f811366f7846a58e6f0f3cc29a985e88e3ed535c
GiwbyAlbatross <giwby.albatross@gmail.com>
2025-07-01 12:24:16 +1000

    Never mind, I meant issue #5

--- run.py
+++ run.py
@@ -4,3 +4,4 @@ from sys import exit, executable, platform # pylint: disable=W0622
 from os import system
 system('clear' if platform != 'win32' else 'cls')
 exit(system(f'{executable} src/main.py'))
+#
